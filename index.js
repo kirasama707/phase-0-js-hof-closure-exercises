ffc34988a3322056b9ec9ad0265b84e6b2d7b562
christian <vagetto707@aol.com>
2022-03-27 10:52:18 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,37 +1,94 @@
-function forEach() {
-
+function forEach(arr, callback) {
+   for(let i = 0; i<arr.length; i++) {
+      callback(arr[i], i, arr) 
+    }
+}
+const arr = [1, 2, 3]
+const fun = function(num) {
+  console.log(num)
 }
 
-function map() {
-
+function map(arr , callback) {
+const newArr = []
+for(let i = 0; i<arr.length; i++) {
+  newArr.push(callback(arr[i]))
+}
+return newArr
 }
 
-function filter() {
-
+function filter(arr, callback) {
+const newArr = []
+for(let i = 0; i<arr.length; i++) {
+  if (callback(arr[i]) === true) {
+      newArr.push(arr[i])
+  }
+}
+return newArr
 }
 
-function every() {
-
+function every(arr, callback) {
+  for (item of arr) {
+    if (callback(item) === false) {
+      return false
+    }
+  }
+return true
 }
 
-function some() {
-
+function some(arr , callback) {
+for ( item of arr) {
+  if (callback(item) === true) {
+    return true
+  }
+}
+return false
 }
 
-function majority() {
-
+function majority(arr, callback) {
+const maj = Math.floor((arr.length / 2) + 1)
+const newArr = []
+for (item of arr) {
+  if (callback(item) === true) {
+     newArr.push(item)
+  }
+}
+  if(newArr.length >=maj) {
+    return true
+  } else {
+    return false
+  }
 }
 
-function once() {
-
+function once(callback) {
+  let isCalled = false 
+  let value;
+  return function(args) {
+    if (isCalled) return value
+    isCalled = true
+    value = callback(args)
+    return value
+  }
 }
 
-function groupBy() {
-
+function groupBy(arr, callback) {
+  const obj = {}
+  for (item of arr) {
+    let key = callback(item)
+    if(obj[key]) {
+      obj[key].push(item)
+    } else {
+      obj[key] = [item]
+    }
+  }
+return obj
 }
 
-function arrayToObject() {
-
+function arrayToObject(arr, callback) {
+  let obj = {}
+  for (item of arr) {
+    obj[item] = callback(item)
+  }
+  return obj
 }
 
 module.exports = { 
